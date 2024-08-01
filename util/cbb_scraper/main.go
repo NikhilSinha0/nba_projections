416253607209c93239b61d80edbd0e4b17390ae5
Nikhil Sinha <nikhilsinha@outlook.com>
2024-08-01 17:58:41 -0400

    Add identity resolution information

--- util/cbb_scraper/main.go
+++ util/cbb_scraper/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
@@ -16,7 +17,7 @@ const (
 func main() {
 
 	var out [][]string
-	rows := []string{"Num", "Player", "Team", "GP", "MPG", "PPG", "FGM", "FGA", "FG%", "3PM", "3PA", "3P%", "FTM", "FTA", "FT%", "ORB", "DRB", "RPG", "APG", "SPG", "BPG", "TOV", "PF", "Year"}
+	rows := []string{"Num", "PlayerID", "Player", "Conference", "Team", "GP", "MPG", "PPG", "FGM", "FGA", "FG%", "3PM", "3PA", "3P%", "FTM", "FTA", "FT%", "ORB", "DRB", "RPG", "APG", "SPG", "BPG", "TOV", "PF", "Year"}
 	out = append(out, rows)
 
 	for i := 2003; i < 2025; i++ {
@@ -31,6 +32,15 @@ func main() {
 		c.OnHTML("tbody>tr", func(e *colly.HTMLElement) {
 			var sub []string
 			e.ForEach("td", func(k int, d *colly.HTMLElement) {
+				d.ForEach("a", func(k1 int, d1 *colly.HTMLElement) {
+					href := d1.Attr("href")
+					parts := strings.Split(href, "/")
+					if parts[1] == "player" {
+						sub = append(sub, strings.Join([]string{parts[2], parts[4]}, "-"))
+					} else {
+						sub = append(sub, parts[3])
+					}
+				})
 				sub = append(sub, d.Text)
 			})
 			sub = append(sub, fmt.Sprintf("%d", i))
